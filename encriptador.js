290c5e2c2c07715e1eb60a017336127f4ac2e185
Rafael Romero <rafa710@gmail.com>
2022-05-10 12:57:09 -0300

    agrega funciones de encriptar, desencriptar y copiar msj

---
+++ encriptador.js
@@ -0,0 +1,60 @@
+function copiarMensaje(){
+  const $resultadoMensaje =  document.querySelector("#resultado-mensaje");
+  document.querySelector("#mensaje-usuario").value = $resultadoMensaje.value;
+  $mensajeUsuario.focus();
+}
+
+const $btnCopiarMensaje = document.querySelector("#btn-copiar-mensaje");
+$btnCopiarMensaje.addEventListener("click", function(event){
+  event.preventDefault();
+  copiarMensaje();
+});
+
+function desencriptarMensaje(mensaje){
+  let mensajeDesencriptado =  mensaje.replaceAll("ufat", "u")
+  mensajeDesencriptado =  mensajeDesencriptado.replaceAll("ober", "o")
+  mensajeDesencriptado =  mensajeDesencriptado.replaceAll("ai", "a")
+  mensajeDesencriptado =  mensajeDesencriptado.replaceAll("imes", "i")
+  mensajeDesencriptado =  mensajeDesencriptado.replaceAll("enter", "e")
+  return mensajeDesencriptado;
+}
+
+const $btnDesencriptar = document.querySelector("#btn-desencriptar");
+$btnDesencriptar.addEventListener("click", function(event){
+  event.preventDefault();
+  const errores= validarTexto($mensajeUsuario.value);
+  if(errores.length > 0){
+    mostrarErrores(errores);  
+    return;
+  }
+  const $resultadoMensaje = document.querySelector("#resultado-mensaje");
+  $resultadoMensaje.textContent = "";
+  const mensajeDesencriptado = desencriptarMensaje($mensajeUsuario.value);
+  mostrarMensaje(mensajeDesencriptado);
+  eliminarMensajeError();
+});
+
+function encriptarMensaje(mensajeDelUsuario){
+  let mensajeEncriptado =  mensajeDelUsuario.replaceAll("e", "enter");
+  mensajeEncriptado =  mensajeEncriptado.replaceAll("i", "imes");
+  mensajeEncriptado =  mensajeEncriptado.replaceAll("a", "ai");
+  mensajeEncriptado =  mensajeEncriptado.replaceAll("o", "ober");
+  mensajeEncriptado =  mensajeEncriptado.replaceAll("u", "ufat");
+  return mensajeEncriptado;
+}
+
+const $mensajeUsuario = document.querySelector("#mensaje-usuario");
+const $btnEncriptar = document.querySelector("#btn-encriptar");
+$btnEncriptar.addEventListener("click", function(event){
+  event.preventDefault();
+  const errores= validarTexto($mensajeUsuario.value);
+  if(errores.length > 0){
+    mostrarErrores(errores);  
+    return;
+  }
+  const mensajeEncriptado = encriptarMensaje($mensajeUsuario.value);
+  mostrarMensaje(mensajeEncriptado);
+  eliminarMensajeError();
+});
+
+
