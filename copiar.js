563a8bb9bb8fca358541b6ce41124b1021d91c0a
Rafael Romero <rafa710@gmail.com>
2022-05-12 05:45:21 -0300

    crea funcionalidad para copiar al portapapeles

---
+++ copiar.js
@@ -0,0 +1,22 @@
+
+function copiarTextoAlPortapapeles(){
+  let textoCopiado = document.querySelector("#resultado-mensaje").innerHTML;
+  navigator.clipboard.writeText(textoCopiado).then(() =>{
+    alert("Texto Copiado en el portapapeles!!!")
+  })
+  .catch(err => {
+    alert("Algo salio MAL!!!", err);
+  })
+}
+
+// El codigo debajo, hace lo mismo que lo que esta arriba, pero utiliza document.execCommand() el 
+// cual se desanconseja su uso...
+// function copiarTextoAlPortapapeles(){
+//   let textoCopiado = document.querySelector("#resultado-mensaje");
+//   textoCopiado.select();
+//   document.execCommand("copy");
+//   alert("Texto Copiado");
+// }
+// document.querySelector("btn-copiar-al-portapapeles").addEventListener("click", copiarTextoAlPortapapeles);
+
+
