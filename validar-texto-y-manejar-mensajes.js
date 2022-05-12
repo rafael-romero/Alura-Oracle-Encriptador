8abb960ccfdf9646fa6c4d12389af3efff3de2ea
Rafael Romero <rafa710@gmail.com>
2022-05-12 05:49:43 -0300

    crea funcion para ocultar botones y resultado
    deja la parte de resultado como viene cuando se carga la pagina desde cero, ocultando elementos

--- validar-texto-y-manejar-mensajes.js
+++ validar-texto-y-manejar-mensajes.js
@@ -1,4 +1,5 @@
 function eliminarMensajeError(){
+  const $mensajeUsuario = document.querySelector("#mensaje-usuario");
   const $cartelError = document.querySelector("#mensaje-error");
   $cartelError.textContent = "";
   $cartelError.classList.add("ocultar");
@@ -6,14 +7,32 @@ function eliminarMensajeError(){
   $mensajeUsuario.value = "";
 }
 
-function mostrarMensaje(mensaje){
-  document.querySelector("#no-se-encontro-imagen").classList.add("ocultar");
-  document.querySelector("#no-se-encontro-texto").classList.add("ocultar");
+function ocultarMensajeResultado(){
+  const $noSeEncontroImagen = document.querySelector("#no-se-encontro-imagen");
+  $noSeEncontroImagen.classList.remove("ocultar");
+  const $noSeEncontroTexto = document.querySelector("#no-se-encontro-texto");
+  $noSeEncontroTexto.classList.remove("ocultar");
   const $resultadoMensaje =  document.querySelector("#resultado-mensaje");
-  $resultadoMensaje.classList.remove("ocultar");
+  // $resultadoMensaje.textContent = "";
+  $resultadoMensaje.classList.add("ocultar");
+  const $btnCopiarMensaje =  document.querySelector("#btn-copiar-mensaje");
+  $btnCopiarMensaje.classList.add("ocultar");
+  const $btnCopiarMensajeAlPortapapeles = document.querySelector("#btn-copiar-al-portapapeles");
+  $btnCopiarMensajeAlPortapapeles.classList.add("ocultar");
+}
+
+function mostrarMensaje(mensaje){
+  const $noSeEncontroImagen = document.querySelector("#no-se-encontro-imagen");
+  $noSeEncontroImagen.classList.add("ocultar");
+  const $noSeEncontroTexto = document.querySelector("#no-se-encontro-texto");
+  $noSeEncontroTexto.classList.add("ocultar");
+  const $resultadoMensaje =  document.querySelector("#resultado-mensaje");
   $resultadoMensaje.textContent = mensaje;
+  $resultadoMensaje.classList.remove("ocultar");
   const $btnCopiarMensaje =  document.querySelector("#btn-copiar-mensaje");
   $btnCopiarMensaje.classList.remove("ocultar");
+  const $btnCopiarMensajeAlPortapapeles = document.querySelector("#btn-copiar-al-portapapeles");
+  $btnCopiarMensajeAlPortapapeles.classList.remove("ocultar");
 }
 
 function mostrarErrores(errores){
